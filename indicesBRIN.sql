b64a62c000853b2cd7deee20ac2ab8eb24a52c1a
patrickpasqualotto <128628026+patrickpasqualotto@users.noreply.github.com>
2024-12-13 01:45:32 +0000

    Update indicesBRIN.sql
    corrigindo a sintaxe que estava errada

--- indicesBRIN.sql
+++ indicesBRIN.sql
@@ -1,27 +1,27 @@
 
 CONSULTA 1
 
-CREATE INDEX CONCURRENTLY idx_concorrente_consulta1 ON corrida (datapedido)
+CREATE INDEX idx_concorrente_consulta1 ON corrida USING brin(datapedido)
 DROP INDEX idx_concorrente_consulta1
   
 CONSULTA 2
 
-CREATE INDEX CONCURRENTLY idx_concorrente_consulta2 ON motorista (nome)
+CREATE INDEX idx_concorrente_consulta2 ON motorista USING brin(nome)
 DROP INDEX idx_concorrente_consulta1
 
 
 CONSULTA 3
 
-CREATE INDEX CONCURRENTLY idx_concorrente_consulta3 ON taxi (modelo)
+CREATE INDEX idx_concorrente_consulta3 ON taxi USING brin (modelo)
 DROP INDEX idx_concorrente_consulta3
 
 CONSULTA 4
 
 
-CREATE INDEX CONCURRENTLY idx_concorrente_consulta4 ON taxi (modelo)
+CREATE INDEX idx_concorrente_consulta4 ON taxi USING brin (modelo)
 DROP INDEX idx_concorrente_consulta4
 
 CONSULTA 5
 
-CREATE INDEX CONCURRENTLY idx_concorrente_consulta5 ON fila (zona)
+CREATE INDEX idx_concorrente_consulta5 ON fila USING brin (zona)
 DROP INDEX idx_concorrente_consulta5
